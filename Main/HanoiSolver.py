beaa29e81c24203f850a9ffdbf4570da01350c6a
Matthew Balshaw <matt@mbm.space>
2015-05-16 02:52:46 +1000

    Working with test data
    Seems to be fully working with test data. Ouputs all required data

--- Main/HanoiSolver.py
+++ Main/HanoiSolver.py
@@ -1,4 +1,5 @@
 """
+Intended for Python 2.7.9
 
 Prototype code to be converted into C for use on an Arduino Uno
 
@@ -8,7 +9,6 @@ based on how long the user would like the tower of hanoi puzzle to last for
 Author: Matthew Balshaw
 E-mail: matt@mbm.space
 Updated: 16/May/2015
-
 """
 
 
@@ -18,17 +18,18 @@ import math
 
 def GatherInputs():
     print "Hello! Welcome to the Python Hanoi Calculator\n"
+    
     invalid = True
-
     while invalid:
         #currDate = raw_input("What is the date today? (DD-MM-YYYY)\n")
         #currTime = raw_input("What is the time? (24hr ##:##)\n")
         currDate = "01-01-2000"
         currTime = "12:00"
+        
         strCurrDateTime = currDate + currTime
         try:
             currDateTime = datetime.datetime.strptime(strCurrDateTime, '%d-%m-%Y%H:%M')
-            print "Does ", currDateTime, " Look right?\n"
+            print "Does ", currDateTime, "Look right?\n"
             invalid = False
         except:
             print "Invalid date/time configuration. Please try again\n\n"
@@ -39,32 +40,59 @@ def GatherInputs():
         #endTime = raw_input("What time would you like the puzzle to be solved? (24hr ##:##)\n")
         endDate = "01-01-2060"
         endTime = "12:00"
+        
         strEndDateTime = endDate + endTime
         try:
             endDateTime = datetime.datetime.strptime(strEndDateTime, '%d-%m-%Y%H:%M')
-            print "Does ", endDateTime, " Look right?\n"
+            print "Does ", endDateTime, "Look right?\n"
             invalid = False
         except:
             print "Invalid date/time configuration. Please try again\n\n"
 
-    print "Successful!\n"
+    invalid = True
+    while invalid:
+        #fastestStepTime = raw_input("Fastest time for the longest step? (Integer please)\n")
+        #desiredStepTime = raw_input("Desired step time?(Integer please)\n")
+        fastestStepTime = 5
+        desiredStepTime = 10.0
+        
+        try:
+            fastestStepTime
+            desiredStepTime
+
+            minStepTime = float(desiredStepTime) * 0.75
+
+            if float(fastestStepTime) < minStepTime:
+                
+                invalid = False
+            else:
+                print fastestStepTime, minStepTime
+                print "Picked invalid step times!\n\n"
+        except:
+            print "Bad Input\n\n"
+
+
+    print "Successful Input!\n\n"
+
+    print "####################DATA####################"
+    
     timeBetween = (endDateTime-currDateTime).total_seconds()
-    print "The seconds between the dates is ", timeBetween, " Seconds"
-
-
-    steptime = 10.0
-
-    stepsApprox = timeBetween/steptime
+    print "The time between the dates is", timeBetween, "Seconds"
+    print "Which is", ((((timeBetween)/60)/60)/24)/365, "Years"
+    
+    stepsApprox = timeBetween/minStepTime
 
     approxDisks = int((math.log(stepsApprox+1))/(math.log(2)))
-    print "Requires around ", approxDisks, " Disks"
+    print "Requires around", approxDisks, "Disks"
 
     stepsNeeded = (math.pow(2, approxDisks)) - 1
 
-    print stepsNeeded
+    print "To solve for", approxDisks, "disks we need", int(stepsNeeded), "steps to complete"
+
+    timePerStep = timeBetween/stepsNeeded
+
+    print "Each step should take", timePerStep, "Seconds"
 
     
-
-
 if __name__ == '__main__':
     GatherInputs()
